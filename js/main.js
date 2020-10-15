de19478b19e0f39c0bec0caced4843dcc40dd6d8
Mihail Nojakshev <Nojakschev@mail.ru>
2020-10-15 15:34:23 +0500

    create: modal & media for modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -25,4 +27,27 @@ var reviewSlider = new Swiper('.reviews-slider', {
 var menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', function () {
   document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+});
+
+var modalButton = document.querySelector('.booking__button');
+modalButton.addEventListener('click', function () {
+  document.querySelector('.modal__overlay').classList.add('modal__overlay--active');
+  document.querySelector('.modal__dialog').classList.add('modal__dialog--active');
+});
+
+var modalClose = document.querySelector('.modal');
+modalClose.addEventListener("click", function(event) {
+  var target = event.target;
+    if (target.classList.contains("close")) {
+      event.preventDefault();
+      document.querySelector('.modal__overlay').classList.remove('modal__overlay--active');
+      document.querySelector('.modal__dialog').classList.remove('modal__dialog--active');
+    } else {
+      target = target.closest(".modal__dialog");
+      if (!target) {
+        document.querySelector('.modal__overlay').classList.remove('modal__overlay--active');
+        document.querySelector('.modal__dialog').classList.remove('modal__dialog--active');
+      }
+    }
+});
 });
